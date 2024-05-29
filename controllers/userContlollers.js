286e5a52e2d6e36ffd02a845397c2f0e0026395b
Oleksandr-Gab <Saniafan1@gmail.com>
2024-05-30 00:05:43 +0300

    go

--- controllers/userContlollers.js
+++ controllers/userContlollers.js
@@ -1,104 +1,133 @@
 import HttpError from "../helpers/HttpError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import gravatar from "gravatar";
+import Jimp from "jimp";
 
 import User from "../models/user.js";
 import {
-    userRegisterSchema,
-    userLoginSchema,
+  userLoginSchema,
+  userRegisterSchema,
 } from "../schemas/usersSchemas.js";
 
 async function registerUser(req, res, next) {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const { error } = userRegisterSchema.validate(req.body);
-    if (typeof error !== "undefined") {
-        return res.status(400).send({message: error.message});
+  const { error } = userRegisterSchema.validate(req.body);
+  if (typeof error !== "undefined") {
+    return res.status(400).send({message: error.message});
+  }
+
+  try {
+    const newUser = await User.findOne({ email });
+
+    if (newUser !== null) {
+      throw HttpError(409, "Email in use");
     }
 
-    try {
-        const newUser = await User.findOne({ email });
+    const passwordHash = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-        if (newUser !== null) {
-            throw HttpError(409, "Email in use");
-        }
+    const user = await User.create({
+      email,
+      password: passwordHash,
+      avatarURL,
+    });
 
-        const passwordHash = await bcrypt.hash(password, 10);
-
-        const user = await User.create({email, password: passwordHash });
-
-        res.status(201).send({
-            user: {
-                email: user.email,
-                subscription: user.subscription,
-            },
-        });
-    } catch (error) {
-        next(error);
-    }
+    res
+      .status(201)
+      .send({ user: { email: user.email, subscription: user.subscription } });
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function loginUser(req, res, next) {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const { error } = userLoginSchema.validate(req.body);
-    if (typeof error !== "undefined") {
-        return res.status(400).send({message: error.message});
+  const { error } = userLoginSchema.validate(req.body);
+  if (typeof error !== "undefined") {
+    return res.status(400).send({message: error.message});
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user === null) {
+      return res.status(401).send({ message: "Email or password is wrong" });
     }
 
-    try {
-        const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
 
-        if (user === null) {
-            return res.status(401).send({ message: "Email or password is wrong" });
-            }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (isMatch === false) {
-            return res.status(401).send({ message: "Email or password is wrong" });
-            }
-
-        const token = jwt.sign(
-            {
-                id: user._id,
-                email: user.email,
-                subscription: user.subscription,
-            },
-            process.env.JWT_SECRET,
-            { expiresIn: "1d" }
-        );
-
-        await User.findByIdAndUpdate(user._id, { token });
-
-        res.status(200).send({
-            token: token,
-            user: { email: email, subscription: user.subscription },
-        });
-    } catch (error) {
-        console.log(error);
-        next(error);
+    if (isMatch === false) {
+      return res.status(401).send({ message: "Email or password is wrong" });
     }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email, subscription: user.subscription },
+      process.env.JWT_SECRET,
+      { expiresIn: 60 * 120 }
+    );
+
+    await User.findByIdAndUpdate(user._id, { token });
+
+    res.status(200).send({
+      token: token,
+      user: { email: email, subscription: user.subscription },
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function logout(req, res, next) {
-    try {
-        await User.findByIdAndUpdate(req.user.id, { token: null });
-        res.status(204).end();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await User.findByIdAndUpdate(req.user.id, { token: null });
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function currentUser(req, res) {
-    const { email, subscription } = req.user;
+  const { email, subscription } = req.user;
 
-    res.send({ email, subscription });
+  res.send({ email, subscription });
+}
+
+async function usersAvatar(req, res, next) {
+  try {
+    const resize = await Jimp.read(req.file.path);
+    await resize.resize(250, 250);
+    resize.write(req.file.path);
+
+    await fs.rename(
+      req.file.path,
+      path.resolve("public/avatars", req.file.filename)
+    );
+
+    const user = await User.findOneAndUpdate(
+      req.user._id,
+      { avatarURL: req.file.filename },
+      { new: true }
+    );
+
+    if (user === null) {
+      return res.status(401).send({ message: "Not authorized" });
+    }
+
+    res.status(200).send({ avatarURL: user.avatarURL });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export default {
-    registerUser,
-    loginUser,
-    logout,
-    currentUser,
+  registerUser,
+  loginUser,
+  logout,
+  currentUser,
+  usersAvatar,
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,8 @@ import express from "express";
 import auditToken from "../middlewares/auditToken.js";
 import UserControllers from "../controllers/userContlollers.js";
 
+import upload from "../middlewares/upload.js";
+
 const authRouter = express.Router();
 const jsonParcer = express.json();
 
@@ -12,5 +14,7 @@ authRouter.post("/login", jsonParcer, UserControllers.loginUser);
 authRouter.post("/logout", auditToken, UserControllers.logout);
 
 authRouter.get("/current", auditToken, jsonParcer, UserControllers.currentUser);
+
+authRouter.patch("/avatars", upload.single("avatar"), auditToken, UserControllers.usersAvatar);
 
 export default authRouter;
